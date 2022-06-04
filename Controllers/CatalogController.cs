356f39ffc7b57dc6ec779ab674b6ff131ce114bb
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-06-04 21:27:55 +0300

    lesson 1 done

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using APS_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APS_MVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Catalog _catalog = new();
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product model)
+        {
+            _catalog.Products.Add(model);
+            return View(_catalog);
+        }
+    }
+}
