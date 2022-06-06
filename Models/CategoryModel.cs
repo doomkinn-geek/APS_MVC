526f82e7baf3a784e731138fa1cc2dda3f68c7f5
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-06-06 21:40:17 +0300

    ...

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APS_MVC.Models
+{
+    public class CategoryModel
+    {
+        public string Price;
+        public List<Category> Categories { get; set; } = new();
+        public int CategoryId { get; set; }
+        [StringLength(20)]
+        public string Name { get;set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
