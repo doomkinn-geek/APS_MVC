0445bf96b1565b75e5fb7e98cf8164ca138ba82d
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-08-05 17:13:33 +0300

    ...

---
+++ Services/AliveNotificatorService.cs
@@ -0,0 +1,32 @@
+﻿namespace ASP_MVC.Services
+{
+    public class AliveNotificatorService : IHostedService, IDisposable
+    {
+        private CancellationToken _stoppingToken;
+        private int executionCount = 0;
+        private Timer _timer;
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(3600));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
